c64ca9fce42239fc5e01480a602dc28d13522212
aebedir <ae.bedirr@gmail.com>
2023-04-04 20:42:19 +0300

    Anasayfa

---
+++ js/app.js
@@ -0,0 +1,52 @@
+function show(){
+    document.querySelector(".email").classList.remove("d-none")
+}
+function not(){
+    document.querySelector(".email").classList.add("d-none")
+}
+
+$("#2").mouseenter(function(){
+    document.querySelector("#services").classList.remove("d-none")        
+});
+$("#services").mouseleave(function(){
+    document.querySelector("#services").classList.add("d-none")    
+});
+
+$("#3").mouseenter(function(){
+    document.querySelector("#services").classList.add("d-none")
+});
+$("#1").mouseenter(function(){
+    document.querySelector("#services").classList.add("d-none")
+});
+
+function add(id){
+    let active = document.getElementsByClassName("active")
+    active[0].classList.remove("active");
+    document.getElementById(id).classList.add("active")
+}
+
+jQuery(document).ready(function() {
+  
+    var btn = $('.up');
+ 
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 100) {
+        btn.removeClass("d-none");
+      } else {
+        btn.addClass("d-none");
+      }
+    });
+ 
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300');
+    });
+ 
+  });
+
+
+
+
+
+
+
